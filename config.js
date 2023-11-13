7ae30db99588ee6159b97bfb124f6d8b3423e678
Joseph Herrera <josephherreraleon@gmail.com>
2023-11-13 11:55:34 -0600

    Added product connection to get all products from database

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-    db: {
-      host: "127.0.0.1:3306",
-      user: "root",
-      password: "toor",
-      database: "Cafe",
-      connectTimeout: 60000
-    },
-    listPerPage: 10,
-  };
-  module.exports = config;
+  db: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "toor",
+    database: "Cafe",
+    connectTimeout: 60000,
+  },
+  listPerPage: 10,
+};
+module.exports = config;
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const port = 3000;
+const productsRouter = require("./routes/products");
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+app.use("/products", productsRouter);
 
-app.listen(3000);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
+});
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
