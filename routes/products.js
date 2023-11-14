50fc6611eea0f5b957daba1134a1ad00e14e666a
Joseph Herrera <josephherreraleon@gmail.com>
2023-11-13 18:00:56 -0600

    Fix error message for creating products

--- routes/products.js
+++ routes/products.js
@@ -16,7 +16,7 @@ router.post("/", async function (req, res, next) {
   try {
     res.json(await products.create(req.body));
   } catch (err) {
-    console.error(`Error while creating programming language`, err.message);
+    console.error(`Error while creating products`, err.message);
     next(err);
   }
 });
